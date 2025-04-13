1887257a0873d9ab0ba143d3391a325e14896e26
trathi9 <trathi9@gatech.edu>
2025-04-13 15:33:07 +0000

    chore: added convnet and mlp based on arch in farsight paper

--- src/models/convnet.py
+++ src/models/convnet.py
@@ -7,6 +7,7 @@ Created on 2025-04-09 12:36:50 Wednesday
 """
 
 import torch
+import torch.functional as F
 from torch import nn
 
 # structure:
@@ -17,10 +18,19 @@ from torch import nn
 #     sigmoid
 
 
-class conv(nn.Module):
-    def __init__(self):
-        super(conv, self).__init__()
-        pass
+class convnet(nn.Module):
+    def __init__(self, input_size):
+        super(convnet, self).__init__()
+        self.fc1 = nn.Linear(in_features=input_size, out_features=289)
+        self.conv1 = nn.Conv2d(1, 19, kernel_size=3)
+        self.fc2 = nn.Linear(in_features=19 * 225, out_features=19)
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        # reshape the output of fc1 for convolution
+        x = x.reshape(-1, 1, int(289**0.5), int(289**0.5))
+        x = F.relu(self.conv1(x))
+        # flatten to be pass through fully connected layer fc2
+        x = torch.flatten(x, 1)
+        x = self.fc2(x)
+        return x
--- src/models/mlp.py
+++ src/models/mlp.py
@@ -6,7 +6,7 @@ Created on 2025-04-09 12:35:53 Wednesday
 @author: Nikhil Kapila
 """
 
-import torch
+import torch.functional as F
 from torch import nn
 
 # structure:
@@ -17,8 +17,12 @@ from torch import nn
 
 
 class mlp(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size):
         super(mlp, self).__init__()
+        self.fc1 = nn.Linear(in_features=input_size, out_features=75)
+        self.fc2 = nn.Linear(in_features=75, out_features=19)
 
     def forward(self):
-        pass
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
