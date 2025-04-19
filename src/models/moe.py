c292ecba0c551c4694bd73c68ec99472f82b5930
Nikhil Kapila <nikhil.kapila@gatech.edu>
2025-04-19 19:12:55 +0400

    feat(moe.py): added dict logging for plots.

--- src/models/moe.py
+++ src/models/moe.py
@@ -10,29 +10,41 @@ from torch import nn
 import torch
 
 class MoE(nn.Module):
-    def __init__(self, experts, input_dim, hidden_dim):
+    def __init__(self, experts:dict, input_dim:int, hidden_dim:int):
         super().__init__()
-        self.experts = experts
+        self.device = 'mps' if torch.backends.mps.is_available() else ('cuda' if torch.cuda.is_available() else 'cpu')
+        # self.expert_list = experts
+        # self.experts = nn.ModuleList([
+        #     e.to(self.device).eval().requires_grad_(False) for e in experts
+        #     ])
+        
+        self.expert_names = list(experts.keys())
+
+        self.experts = nn.ModuleDict({
+            name: e.to(self.device).eval().requires_grad_(False)
+            for name, e in experts.items()
+            })
+
         self.gating_network = nn.Sequential(
             nn.Linear(in_features=input_dim, out_features=hidden_dim),
             nn.ReLU(),
             nn.Linear(in_features=hidden_dim, out_features=len(experts))
-        )
-        self.device = 'mps' if torch.backends.mps.is_available() else ('cuda' if torch.cuda.is_available() else 'cpu')
+        ).to(self.device)
 
 
     def forward(self, x):
         gate_logits = self.gating_network(x)
         expert_weights = torch.softmax(gate_logits, dim=1)  # n * number of experts
-        expert_outputs = []                                 
-
-        for expert in self.experts:
-            expert.eval()
-            with torch.no_grad():
-                out = expert(x)
-                expert_outputs.append(out)
+        
+        expert_outputs = []
+        with torch.no_grad():
+            for name in self.expert_names:
+                expert_outputs.append(self.experts[name](x))
 
         o = torch.stack(expert_outputs, dim=0)   # number of experts * n * output_size
         w = expert_weights.T.unsqueeze(dim=-1)
+        out = torch.sum(o * w, dim=0)
 
-        return torch.sum(o * w, dim=0)
+        mean_w = expert_weights.mean(dim=0) # mean over batch
+        w_dict = {name: mean_w[i].item() for i,name in enumerate(self.expert_names)}
+        return out, expert_weights, w_dict
